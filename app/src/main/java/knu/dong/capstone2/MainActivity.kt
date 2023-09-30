97a382cad06afbe5297cfdfa00797f2d5001dfee
gidskql6671 <gidskql6671@gmail.com>
2023-09-30 10:46:25 +0900

    init

---
+++ app/src/main/java/knu/dong/capstone2/MainActivity.kt
@@ -0,0 +1,11 @@
+package knu.dong.capstone2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
