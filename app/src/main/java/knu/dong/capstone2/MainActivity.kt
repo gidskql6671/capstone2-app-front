258b04363e8cbaccc15e3d4566ad82d906d1e33e
gidskql6671 <gidskql6671@gmail.com>
2023-09-30 12:41:52 +0900

    View Binding 추가

--- app/src/main/java/knu/dong/capstone2/MainActivity.kt
+++ app/src/main/java/knu/dong/capstone2/MainActivity.kt
@@ -2,10 +2,13 @@ package knu.dong.capstone2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import knu.dong.capstone2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
     }
 }
