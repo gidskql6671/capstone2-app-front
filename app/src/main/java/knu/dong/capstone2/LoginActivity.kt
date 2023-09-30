0e5f566f4c77d3d51eec78ecbc290a13e3c2dcef
gidskql6671 <gidskql6671@gmail.com>
2023-09-30 14:54:54 +0900

    회원가입 UI 구현

--- app/src/main/java/knu/dong/capstone2/LoginActivity.kt
+++ app/src/main/java/knu/dong/capstone2/LoginActivity.kt
@@ -1,14 +1,21 @@
 package knu.dong.capstone2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import knu.dong.capstone2.databinding.ActivityLoginBinding
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityLoginBinding.inflate(layoutInflater);
-        setContentView(binding.root);
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
